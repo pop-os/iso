901452e08c1bd540cc3fb6134a81976ff2fd6511
Jeremy Soller <jackpot51@gmail.com>
2017-05-09 11:59:23 -0600

    Use magical dconf setting files

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 export HOME=/root
 export LC_ALL=C
@@ -7,14 +7,29 @@ add-apt-repository -y ppa:system76-dev/stable
 apt update -y
 apt install -y system76-driver system76-pop-theme
 
-dconf write /org/gnome/desktop/interface/gtk-theme "'Pop'"
-dconf write /org/gnome/desktop/interface/icon-theme "'Pop'"
-dconf write /org/gnome/desktop/interface/cursor-theme "'Pop'"
+cat > /etc/dconf/profile/system76 <<EOF
+user-db:user
+system-db:system76
+EOF
 
-dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Fira Sans Semi-Bold 10'"
-dconf write /org/gnome/desktop/interface/font-name "'Fira Sans Semi-Light 10'"
-dconf write /org/gnome/desktop/interface/document-font-name "'Roboto Slab 11'"
-dconf write /org/gnome/desktop/interface/monospace-font-name "'Fira Mono 11'"
+mkdir -p /etc/dconf/db/system76.d
+cat > /etc/dconf/db/system76.d/00-interface <<EOF
+[org/gnome/desktop/interface]
+gtk-theme='Pop'
+icon-theme='Pop'
+cursor-theme='Pop'
+font-name='Fira Sans Semi-Light 10'
+document-font-name='Roboto Slab 11'
+monospace-font-name='Fira Mono 11'
+
+[org/gnome/desktop/wm/preferences]
+titlebar-font='Fira Sans Semi-Bold 10'
+EOF
+
+dconf update
+
+echo "Please make your changes, then exit"
+bash
 
 #apt full-upgrade
 apt clean -y
