538b5508ba507279aa0d8a9e584dea250c16fab1
Jeremy Soller <jackpot51@gmail.com>
2017-05-09 16:25:40 -0600

    Purge ubuntu gnome packages before installing system76-default-settings

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -5,7 +5,16 @@ export LC_ALL=C
 
 add-apt-repository -y ppa:system76-dev/daily
 apt update -y
-apt purge -y ubuntu-gnome-default-settings
-apt install -y system76-driver system76-default-settings
+apt purge -y \
+    ubuntu-gnome-desktop \
+    ubuntu-gnome-default-settings \
+    plymouth-theme-ubuntu-gnome-logo \
+    plymouth-theme-ubuntu-gnome-text
+apt autoremove -y --purge
+apt install -y \
+    system76-driver \
+    system76-default-settings
 #apt full-upgrade
 apt clean -y
+
+bash
