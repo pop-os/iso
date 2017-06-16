d9decb9ce94b707571372845c21a9843bcba929c
Jeremy Soller <jackpot51@gmail.com>
2017-06-16 16:03:24 -0600

    Update os-release

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -7,7 +7,7 @@ dbus-uuidgen > /var/lib/dbus/machine-id
 
 touch /var/cache/system76-pre-master.marker
 
-for repo in $REPOS
+for repo in ${DISTRO_REPOS}
 do
     echo "Adding repository '$repo'"
     add-apt-repository -y "$repo"
@@ -15,7 +15,7 @@ done
 
 apt update -y
 
-#sudo apt upgrade
+sudo apt upgrade -y
 
 if [ $# -gt 0 ]
 then
@@ -26,6 +26,21 @@ fi
 apt clean -y
 
 dpkg-query -W --showformat='${Package} ${Version}\n' > /filesystem.manifest
+
+cat > /etc/os-release <<EOF
+NAME="${DISTRO_NAME}"
+VERSION="17.04 (Zesty Zapus)"
+ID=${DISTRO_CODE}
+ID_LIKE=debian ubuntu
+PRETTY_NAME="${DISTRO_NAME} 17.04"
+VERSION_ID="17.04"
+HOME_URL="http://www.system76.com"
+SUPPORT_URL="http://support.system76.com"
+BUG_REPORT_URL="https://github.com/system76/distro"
+PRIVACY_POLICY_URL="https://system76.com/privacy"
+VERSION_CODENAME=zesty
+UBUNTU_CODENAME=zesty
+EOF
 
 rm /usr/lib/ubiquity/plugins/ubi-usersetup.py
 
