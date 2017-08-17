014e554ffa8e96cf422a0346a6d517d7fd46e3b6
Jeremy Soller <jackpot51@gmail.com>
2017-08-17 13:02:31 -0600

    Add buildchain config

---
+++ buildchain.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+shopt -s nullglob
+
+export LC=C.UTF-8
+export SOURCE_DATE_EPOCH="$(git log -1 --format='%ct')"
+
+function usage {
+    echo "buildchain.sh [destination]" >&2
+}
+
+function msg {
+    echo -e "\e[1m""$@""\e[0m" >&2
+}
+
+if [ -z "$1" ]
+then
+    usage
+    exit 1
+fi
+DIR="$(realpath "$1")"
+
+mkdir -p "$DIR"
+
+export DISTRO_EPOCH="${SOURCE_DATE_EPOCH}"
+export DISTRO_VERSION=17.10
+
+msg "clean"
+make clean
+
+msg "build"
+make
+
+pushd "build/${DISTRO_VERSION}" > /dev/null
+    mv -v pop-os.iso "$DIR"
+popd > /dev/null
