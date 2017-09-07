4220f8c34ec9b4e34891c3d6dc2ab38ad5b8fa20
Jeremy Soller <jeremy@system76.com>
2017-09-07 10:45:41 -0600

    Remove hack for default session

--- mk/chroot.mk
+++ mk/chroot.mk
@@ -102,9 +102,6 @@ $(BUILD)/squashfs: $(BUILD)/chroot
 	sudo rm -f "$@.partial/usr/lib/ubiquity/plugins/ubi-usersetup.py"
 	sudo rm -f "$@.partial/usr/lib/ubiquity/plugins/ubi-wireless.py"
 
-	# Set default xsession
-	sudo sed -i 's|^XSession=.*|XSession=pop-xorg|' "$@.partial/var/lib/AccountsService/users/$(DISTRO_USER)"
-
 	sudo mv "$@.partial" "$@"
 
 $(BUILD)/squashfs.tag: $(BUILD)/squashfs
