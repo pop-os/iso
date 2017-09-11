914d4d84fb061a639dc85d90dd514e310448e516
Jeremy Soller <jeremy@system76.com>
2017-09-11 14:19:48 -0600

    Update pool

--- mk/config.mk
+++ mk/config.mk
@@ -49,14 +49,33 @@ RM_PKGS=\
 MAIN_POOL=\
 	b43-fwcutter \
 	dkms \
+	fakeroot \
+	gcc \
+	gcc-7 \
 	grub-efi \
 	grub-efi-amd64 \
 	grub-efi-amd64-bin \
 	grub-efi-amd64-signed \
+	libasan4 \
+	libatomic1 \
+	libc6-dev \
+	libc-dev-bin \
+	libcilkrts5 \
+	libfakeroot \
+	libgcc-7-dev \
+	libitm1 \
+	liblsan0 \
+	libmpx2 \
+	libquadmath0 \
+	libtsan0 \
+	libubsan0 \
 	libuniconf4.6 \
 	libwvstreams4.6-base \
 	libwvstreams4.6-extras \
+	linux-libc-dev \
 	lupin-support \
+	make \
+	manpages-dev \
 	mouseemu \
 	oem-config \
 	oem-config-gtk \
@@ -66,6 +85,7 @@ MAIN_POOL=\
 	shim-signed \
 	user-setup \
 	wvdial
+
 
 # Additional pool packages from the restricted set of packages
 RESTRICTED_POOL=\
