2a839ed563d26c6f4097f357c8740e09afd0a6f0
Jeremy Soller <jeremy@system76.com>
2018-03-05 10:13:34 -0700

    Comment out language pkgs

--- mk/language.mk
+++ mk/language.mk
@@ -13,101 +13,101 @@ LANGUAGES=\
 	zh-hant
 
 LANGUAGE_PKGS=\
-	fonts-arphic-ukai \
-	fonts-arphic-uming \
-	fonts-beng \
-	fonts-beng-extra \
-	fonts-dejavu-core \
-	fonts-deva \
-	fonts-deva-extra \
-	fonts-droid-fallback \
-	fonts-freefont-ttf \
-	fonts-gargi \
-	fonts-gubbi \
-	fonts-gujr \
-	fonts-gujr-extra \
-	fonts-guru \
-	fonts-guru-extra \
-	fonts-indic \
-	fonts-kacst \
-	fonts-kacst-one \
-	fonts-kalapi \
-	fonts-khmeros-core \
-	fonts-knda \
-	fonts-lao \
-	fonts-liberation \
-	fonts-lklug-sinhala \
-	fonts-lohit-beng-assamese \
-	fonts-lohit-beng-bengali \
-	fonts-lohit-deva \
-	fonts-lohit-gujr \
-	fonts-lohit-guru \
-	fonts-lohit-knda \
-	fonts-lohit-mlym \
-	fonts-lohit-orya \
-	fonts-lohit-taml \
-	fonts-lohit-taml-classical \
-	fonts-lohit-telu \
-	fonts-mlym \
-	fonts-nakula \
-	fonts-navilu \
-	fonts-nanum \
-	fonts-noto-cjk \
-	fonts-noto-mono \
-	fonts-opensymbol \
-	fonts-orya \
-	fonts-orya-extra \
-	fonts-pagul \
-	fonts-sahadeva \
-	fonts-samyak-deva \
-	fonts-samyak-gujr \
-	fonts-samyak-mlym \
-	fonts-samyak-taml \
-	fonts-sarai \
-	fonts-sil-abyssinica \
-	fonts-sil-padauk \
-	fonts-smc \
-	fonts-smc-anjalioldlipi \
-	fonts-smc-chilanka \
-	fonts-smc-dyuthi \
-	fonts-smc-karumbi \
-	fonts-smc-keraleeyam \
-	fonts-smc-manjari \
-	fonts-smc-meera \
-	fonts-smc-rachana \
-	fonts-smc-raghumalayalamsans \
-	fonts-smc-suruma \
-	fonts-smc-uroob \
-	fonts-symbola \
-	fonts-takao-pgothic \
-	fonts-taml \
-	fonts-telu \
-	fonts-telu-extra \
-	fonts-thai-tlwg \
-	fonts-tibetan-machine \
-	fonts-tlwg-garuda \
-	fonts-tlwg-garuda-ttf \
-	fonts-tlwg-kinnari \
-	fonts-tlwg-kinnari-ttf \
-	fonts-tlwg-laksaman \
-	fonts-tlwg-laksaman-ttf \
-	fonts-tlwg-loma \
-	fonts-tlwg-loma-ttf \
-	fonts-tlwg-mono \
-	fonts-tlwg-mono-ttf \
-	fonts-tlwg-norasi \
-	fonts-tlwg-norasi-ttf \
-	fonts-tlwg-purisa \
-	fonts-tlwg-purisa-ttf \
-	fonts-tlwg-sawasdee \
-	fonts-tlwg-sawasdee-ttf \
-	fonts-tlwg-typewriter \
-	fonts-tlwg-typewriter-ttf \
-	fonts-tlwg-typist \
-	fonts-tlwg-typist-ttf \
-	fonts-tlwg-typo \
-	fonts-tlwg-typo-ttf \
-	fonts-tlwg-umpush \
-	fonts-tlwg-umpush-ttf \
-	fonts-tlwg-waree \
-	fonts-tlwg-waree-ttf
+	# fonts-arphic-ukai \
+	# fonts-arphic-uming \
+	# fonts-beng \
+	# fonts-beng-extra \
+	# fonts-dejavu-core \
+	# fonts-deva \
+	# fonts-deva-extra \
+	# fonts-droid-fallback \
+	# fonts-freefont-ttf \
+	# fonts-gargi \
+	# fonts-gubbi \
+	# fonts-gujr \
+	# fonts-gujr-extra \
+	# fonts-guru \
+	# fonts-guru-extra \
+	# fonts-indic \
+	# fonts-kacst \
+	# fonts-kacst-one \
+	# fonts-kalapi \
+	# fonts-khmeros-core \
+	# fonts-knda \
+	# fonts-lao \
+	# fonts-liberation \
+	# fonts-lklug-sinhala \
+	# fonts-lohit-beng-assamese \
+	# fonts-lohit-beng-bengali \
+	# fonts-lohit-deva \
+	# fonts-lohit-gujr \
+	# fonts-lohit-guru \
+	# fonts-lohit-knda \
+	# fonts-lohit-mlym \
+	# fonts-lohit-orya \
+	# fonts-lohit-taml \
+	# fonts-lohit-taml-classical \
+	# fonts-lohit-telu \
+	# fonts-mlym \
+	# fonts-nakula \
+	# fonts-navilu \
+	# fonts-nanum \
+	# fonts-noto-cjk \
+	# fonts-noto-mono \
+	# fonts-opensymbol \
+	# fonts-orya \
+	# fonts-orya-extra \
+	# fonts-pagul \
+	# fonts-sahadeva \
+	# fonts-samyak-deva \
+	# fonts-samyak-gujr \
+	# fonts-samyak-mlym \
+	# fonts-samyak-taml \
+	# fonts-sarai \
+	# fonts-sil-abyssinica \
+	# fonts-sil-padauk \
+	# fonts-smc \
+	# fonts-smc-anjalioldlipi \
+	# fonts-smc-chilanka \
+	# fonts-smc-dyuthi \
+	# fonts-smc-karumbi \
+	# fonts-smc-keraleeyam \
+	# fonts-smc-manjari \
+	# fonts-smc-meera \
+	# fonts-smc-rachana \
+	# fonts-smc-raghumalayalamsans \
+	# fonts-smc-suruma \
+	# fonts-smc-uroob \
+	# fonts-symbola \
+	# fonts-takao-pgothic \
+	# fonts-taml \
+	# fonts-telu \
+	# fonts-telu-extra \
+	# fonts-thai-tlwg \
+	# fonts-tibetan-machine \
+	# fonts-tlwg-garuda \
+	# fonts-tlwg-garuda-ttf \
+	# fonts-tlwg-kinnari \
+	# fonts-tlwg-kinnari-ttf \
+	# fonts-tlwg-laksaman \
+	# fonts-tlwg-laksaman-ttf \
+	# fonts-tlwg-loma \
+	# fonts-tlwg-loma-ttf \
+	# fonts-tlwg-mono \
+	# fonts-tlwg-mono-ttf \
+	# fonts-tlwg-norasi \
+	# fonts-tlwg-norasi-ttf \
+	# fonts-tlwg-purisa \
+	# fonts-tlwg-purisa-ttf \
+	# fonts-tlwg-sawasdee \
+	# fonts-tlwg-sawasdee-ttf \
+	# fonts-tlwg-typewriter \
+	# fonts-tlwg-typewriter-ttf \
+	# fonts-tlwg-typist \
+	# fonts-tlwg-typist-ttf \
+	# fonts-tlwg-typo \
+	# fonts-tlwg-typo-ttf \
+	# fonts-tlwg-umpush \
+	# fonts-tlwg-umpush-ttf \
+	# fonts-tlwg-waree \
+	# fonts-tlwg-waree-ttf
