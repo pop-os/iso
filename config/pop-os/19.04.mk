ba5d0f905e5ed6efda7ca90e5155de59486a0b9e
Jeremy Soller <jeremy@system76.com>
2019-04-10 08:58:12 -0600

    Remove mozc-utils-gui from ISO

--- config/pop-os/19.04.mk
+++ config/pop-os/19.04.mk
@@ -44,6 +44,7 @@ LIVE_PKGS=\
 # Packages to remove from installed system (usually installed as Recommends)
 RM_PKGS=\
 	imagemagick-6.q16 \
+	mozc-utils-gui \
 	pop-installer-session \
 	snapd \
 	unattended-upgrades \
