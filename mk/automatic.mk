5eb33fb8c317f28730441afd8b929a7fb04f307f
Jeremy Soller <jeremy@system76.com>
2019-05-14 13:30:27 -0600

    Set CASPER_PATH to casper until distinst can be updated

--- mk/automatic.mk
+++ mk/automatic.mk
@@ -4,7 +4,7 @@ ISO=$(BUILD)/$(ISO_NAME).iso
 TAR=$(BUILD)/$(ISO_NAME).tar
 USB=$(BUILD)/$(ISO_NAME).img
 
-CASPER_PATH=casper_$(ISO_NAME)
+CASPER_PATH=casper
 
 SED=\
 	s|CASPER_PATH|$(CASPER_PATH)|g; \
