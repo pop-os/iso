8a19e3414b540953048a34e89257c9bf469cbd5b
Jeremy Soller <jeremy@system76.com>
2019-07-08 11:14:39 -0600

    Build with linux-generic-hwe-18.04-edge on 18.04

--- config/pop-os/18.04.mk
+++ config/pop-os/18.04.mk
@@ -18,8 +18,8 @@ DISTRO_REPOS+=\
 
 # Packages to install
 DISTRO_PKGS=\
-	linux-generic-hwe-18.04 \
-	linux-signed-generic-hwe-18.04 \
+	linux-generic-hwe-18.04-edge \
+	linux-signed-generic-hwe-18.04-edge \
 	ubuntu-minimal \
 	ubuntu-standard \
 	pop-desktop \
