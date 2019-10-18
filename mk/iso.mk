03206b597412bee2febf3bde54b49a8b5cd74955
Jeremy Soller <jeremy@system76.com>
2019-10-18 10:20:50 -0600

    Revert "Fix creation of recursive partition"
    This reverts commit 5848f76992a90e4f1bd7666a19f1e8a0f431e8e6.

--- mk/iso.mk
+++ mk/iso.mk
@@ -98,7 +98,7 @@ $(BUILD)/grub:
 	cp -r "data/efi/shimx64.efi.signed" "$(BUILD)/iso/efi/boot/bootx64.efi"
 	cp -r "data/efi/gcdx64.efi.signed" "$(BUILD)/iso/efi/boot/grubx64.efi"
 
-	mkfs.vfat -C "$@.partial/efi.img" 4096
+	mformat -C -f 2880 -L 16 -i "$@.partial/efi.img" ::
 	mcopy -s -i "$@.partial/efi.img" "$(BUILD)/iso/efi" ::/
 
 	touch "$@.partial"
