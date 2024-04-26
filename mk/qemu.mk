36da78785c4eeb02154bba84865708c8e8617b17
Jeremy Soller <jackpot51@gmail.com>
2024-04-26 16:12:08 -0600

    Refactor qemu.mk to support other arches

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -1,51 +1,47 @@
+ifeq ($(DISTRO_ARCH),amd64)
+QEMU=qemu-system-x86_64
 QEMUFLAGS=\
 	-enable-kvm \
 	-m 4G \
 	-smp 8 \
 	-vga qxl
+ifeq ($(efi),no)
+QEMUFLAGS+=-bios /usr/share/OVMF/OVMF_CODE.fd
+BOOTLOADER=UEFI
+else
+BOOTLOADER=BIOS
+endif
+else ifeq ($(DISTRO_ARCH),arm64)
+QEMU=qemu-system-aarch64
+QEMUFLAGS=\
+	-m 4G \
+	-smp 8 \
+	-vga qxl
+BOOTLOADER=UEFI
+else
+$(error unknown DISTRO_ARCH $(DISTRO_ARCH))
+endif
 
 $(BUILD)/%.img:
 	mkdir -p $(BUILD)
 	qemu-img create -f qcow2 "$@.partial" 64G
-
 	mv "$@.partial" "$@"
 
-qemu_bios: $(ISO) $(BUILD)/qemu.img
+qemu: $(ISO) $(BUILD)/qemu.img
 	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) BIOS ISO" \
+		-name "$(DISTRO_NAME) $(DISTRO_VERSION) $(DISTRO_ARCH) $(BOOTLOADER) ISO" \
 		-hda $(BUILD)/qemu.img \
 		-boot d -cdrom "$<"
 
-qemu_bios_hd: $(BUILD)/qemu.img
+qemu_hd: $(BUILD)/qemu.img
 	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) BIOS HD" \
-		-enable-kvm -m 2048 -vga qxl \
+		-name "$(DISTRO_NAME) $(DISTRO_VERSION) $(DISTRO_ARCH) $(BOOTLOADER) HD" \
 		-hda $(BUILD)/qemu.img
 
-qemu_bios_usb: $(USB) $(BUILD)/qemu.img
+qemu_usb: $(ISO) $(BUILD)/qemu.img
 	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) BIOS USB" \
-		-hda "$<" \
-		-hdb $(BUILD)/qemu.img \
-
-qemu_uefi: $(ISO) $(BUILD)/qemu_uefi.img
-	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) UEFI ISO" \
-		-bios /usr/share/OVMF/OVMF_CODE.fd \
-		-hda $(BUILD)/qemu_uefi.img \
-		-boot d -cdrom "$<"
-
-qemu_uefi_hd: $(BUILD)/qemu_uefi.img
-	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) UEFI HD" \
-		-bios /usr/share/OVMF/OVMF_CODE.fd \
-		-hda $(BUILD)/qemu_uefi.img
-
-qemu_uefi_usb: $(USB) $(BUILD)/qemu_uefi.img
-	qemu-system-x86_64 $(QEMUFLAGS) \
-		-name "$(DISTRO_NAME) $(DISTRO_VERSION) UEFI USB" \
-		-bios /usr/share/OVMF/OVMF_CODE.fd \
-		-hda $(BUILD)/qemu_uefi.img \
+		-name "$(DISTRO_NAME) $(DISTRO_VERSION) $(DISTRO_ARCH) $(BOOTLOADER) USB" \
+		-hda $(BUILD)/qemu.img \
 		-boot d -drive if=none,id=img,file="$<" \
 		-device nec-usb-xhci,id=xhci \
 		-device usb-storage,bus=xhci.0,drive=img
